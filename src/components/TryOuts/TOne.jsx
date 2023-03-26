0558013f0d3e6b91dc6e3edd50fcec5530ee0f64
Issiedoesit <105283590+Issiedoesit@users.noreply.github.com>
2023-03-27 00:03:33 +0100

    1 binaries uploaded

--- src/components/TryOuts/TOne.jsx
+++ src/components/TryOuts/TOne.jsx
@@ -1,13 +1,14 @@
 import React, { useEffect } from "react"
 import {gsap} from "gsap"
 import {ScrollTrigger} from "gsap/ScrollTrigger"
-import Rabbit from './../../assets/media/gifs/Rabbit-with-kisses.gif'
+import Rabbit from './../../assets/media/gifs/rabbit-sprite-sheet.png'
 
 
 
 
 
 const TOne = () => {
+  gsap.registerPlugin(ScrollTrigger)
 
     useEffect(()=>{
       const ctx = gsap.context(()=>{
@@ -17,6 +18,7 @@ const TOne = () => {
       gsap.to(".viewer", {
       backgroundPosition: (-offset_value * frame_count * 2) + "px 50%",
       ease: "steps(" + frame_count + ")", // use a stepped ease for the sprite sheet
+      // duration:4,
       scrollTrigger: {
         trigger: ".scene",
         start: "top top",
@@ -26,10 +28,20 @@ const TOne = () => {
       }
 });
       })
+
+      return () => ctx.revert()
     }, [])
 
     return(
-      <div className="w-full h-screen">  <img src={Rabbit} className="w-full h-screen" /></div>
+      <section>
+        <div className="h-screen w-full bg-slate-500 text-4xl flex item-center justify-center"><p>Start</p></div>
+        <div class="scene section" id="sticky">
+          <div className="w-full h-screen viewer">  
+          {/* <img src={Rabbit} className="w-full h-screen" /> */}
+          </div>
+        </div>
+        <div className="h-screen w-full bg-slate-500 text-4xl flex item-center justify-center">End</div>
+      </section>
     )
 }
 
