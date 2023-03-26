978894692acdca82c69f47370d6b376a436f776d
Issiedoesit <105283590+Issiedoesit@users.noreply.github.com>
2023-03-26 22:07:58 +0100

    add delay to cards

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -14,6 +14,15 @@ const Home = () => {
 
   useEffect(() => {
     const ctx = gsap.context(() => {
+
+      let tl = gsap.timeline({
+        repeat:-1,
+        yoyo:true
+      })
+      tl.fromTo(".arrow", {opacity:0}, {opacity:1})
+      tl.to(".arrow-path", {fill:'cyan'})
+      tl.to(".arrow-path", {fill:'teal'})
+
       const heroOneAnim = () => {
         let tl = gsap.timeline({
           scrollTrigger: {
@@ -27,6 +36,7 @@ const Home = () => {
           },
         });
 
+        tl.to(".arrow", {display:'none'})
         tl.fromTo(
           '.initial-text',
           { y: 100, opacity: 0, scaleX: 0 },
@@ -88,11 +98,14 @@ const Home = () => {
     hidden:{
       clipPath:'polygon(0 0, 100% 0, 100% 0, 0 0)'
     },
-    visible:{
-      clipPath:'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
-      transition:{
+    visible: (i) => {
+      return{
+        clipPath:'polygon(0 0, 100% 0, 100% 100%, 0 100%)',
+        transition:{
         duration:2,
-        type:'spring'
+        type:'spring',
+        delay:i*0.5
+      }
       }
     }
   }
@@ -100,6 +113,9 @@ const Home = () => {
   return (
     <div className="home--wrap">
       <div className="cover--all fixed top-0 left-0 h-screen w-full bg-black flex flex-col items-center justify-center text-center p z-30">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="20" height="20" className="arrow animate-bounce fixed top-[50%] left-[fiftyPercent] -translate-y-[50%] -translate-x-[50%]">
+<path className="arrow-path" fill="white" d="M483.2,192.2c-20.5-20.5-53.5-20.8-73.7-0.6L257,344.1L104.5,191.6c-20.2-20.2-53.2-19.9-73.7,0.6  c-20.5,20.5-20.8,53.5-0.6,73.7l190,190c10.1,10.1,23.4,15.1,36.8,15c13.3,0.1,26.7-4.9,36.8-15l190-190  C503.9,245.7,503.7,212.7,483.2,192.2z"/>
+</svg>
         <div className="space-y-3">
           <p className="initial-text text-4xl">New Dawn</p>
           <p className="initial-text text-4xl">New Day</p>
@@ -144,14 +160,14 @@ const Home = () => {
       
 
       <div
-            className="boxes grid md:grid-cols-2 lg:grid-cols-3 gap-y-10 py-20 text-left"
+            className="boxes grid md:grid-cols-2 lg:grid-cols-3 auto-cols-fr gap-y-10 py-20 text-left"
             ref={boxRef}
           >
             <motion.div variants={boxVariant} initial="hidden" whileInView="visible" className="box one">
               <img
                 src="https://images.unsplash.com/photo-1679214110740-15f5ed4d7caa?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Nzk4Mzg1NTY&ixlib=rb-4.0.3&q=85"
                 alt="img"
-                className="aspect-square"
+                className="h-80 w-full object-cover"
               />
               <div className="bg-white px-2 py-5 text-black">
                 <h3 className="text-xl font-black one-lined-text">
@@ -160,11 +176,11 @@ const Home = () => {
                 <p className="pt-1 text-sm font-medium">$ 23</p>
               </div>
             </motion.div>
-            <motion.div variants={boxVariant} initial="hidden" whileInView="visible" className="box two">
+            <motion.div variants={boxVariant} initial="hidden" whileInView="visible" custom={0.5} className="box two">
               <img
                 src="https://images.unsplash.com/photo-1679349909423-6f5ffad19302?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Nzk4NDA0OTY&ixlib=rb-4.0.3&q=85"
                 alt="img"
-                className="aspect-square"
+                className="h-80 w-full object-cover"
               />
               <div className="bg-white px-2 py-5 text-black">
                 <h3 className="text-xl font-black one-lined-text">
@@ -173,11 +189,11 @@ const Home = () => {
                 <p className="pt-1 text-sm font-medium">$ 43.87</p>
               </div>
             </motion.div>
-            <motion.div variants={boxVariant} initial="hidden" whileInView="visible" className="box three">
+            <motion.div variants={boxVariant} initial="hidden" whileInView="visible" custom={1} className="box three">
               <img
                 src="https://images.unsplash.com/photo-1678314530817-68b4966f1b07?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Nzk4NTIyOTA&ixlib=rb-4.0.3&q=85"
                 alt="img"
-                className="aspect-square"
+                className="h-80 w-full object-cover"
               />
               <div className="bg-white px-2 py-5 text-black">
                 <h3 className="text-xl font-black one-lined-text">
@@ -186,11 +202,11 @@ const Home = () => {
                 <p className="pt-1 text-sm font-medium">$ 23</p>
               </div>
             </motion.div>
-            <motion.div variants={boxVariant} initial="hidden" whileInView="visible" className="box four">
+            <motion.div variants={boxVariant} initial="hidden" whileInView="visible" custom={1.5} className="box four">
               <img
                 src="https://images.unsplash.com/photo-1678802676747-308470207d98?crop=entropy&cs=srgb&fm=jpg&ixid=MnwzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE2Nzk4NTIzMjU&ixlib=rb-4.0.3&q=85"
                 alt="img"
-                className="aspect-square"
+                className="h-80 w-full object-cover"
               />
               <div className="bg-white px-2 py-5 text-black">
                 <h3 className="text-xl font-black one-lined-text">
