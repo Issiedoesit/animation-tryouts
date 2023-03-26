2f5b08d9251f534c52c86312e805404f83491d33
Issiedoesit <105283590+Issiedoesit@users.noreply.github.com>
2023-03-26 22:54:59 +0100

    1 binaries uploaded

--- src/components/TryOuts/TOne.jsx
+++ src/components/TryOuts/TOne.jsx
@@ -1,12 +1,35 @@
-import React from "react"
+import React, { useEffect } from "react"
+import {gsap} from "gsap"
+import {ScrollTrigger} from "gsap/ScrollTrigger"
+import Rabbit from './../../assets/media/gifs/Rabbit-with-kisses.gif'
 
 
 
 
 
 const TOne = () => {
+
+    useEffect(()=>{
+      const ctx = gsap.context(()=>{
+        var frame_count  = 42,
+        offset_value = 100;
+
+      gsap.to(".viewer", {
+      backgroundPosition: (-offset_value * frame_count * 2) + "px 50%",
+      ease: "steps(" + frame_count + ")", // use a stepped ease for the sprite sheet
+      scrollTrigger: {
+        trigger: ".scene",
+        start: "top top",
+        end: "+=" + (frame_count * offset_value),
+        pin: true,
+        scrub: true
+      }
+});
+      })
+    }, [])
+
     return(
-      <div>  <img src="https://i.imgur.com/RY2vTBQ.gif" /></div>
+      <div className="w-full h-screen">  <img src={Rabbit} className="w-full h-screen" /></div>
     )
 }
 
